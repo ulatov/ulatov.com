3093348a0158377be646a9e5a08fa0e027f4a556
ulatov <53035688+ulatov@users.noreply.github.com>
2021-04-29 10:23:22 +0500

    new style for circles

--- index.js
+++ index.js
@@ -1,12 +1,7 @@
 const elemBody = document.getElementsByTagName("body")[0]
 
 function deleteElemTimeout(el) {
-  window.setTimeout(() => {
-    let root = document.querySelector(":root")
-    console.log(el.style)
-    root.style.setProperty("--bg-color", el.style.background)
-    el.parentElement.removeChild(el)
-  }, 3000)
+  window.setTimeout(() => el.parentElement.removeChild(el), 3000)
 }
 
 function makeCircle(x,y) {
@@ -15,12 +10,10 @@ function makeCircle(x,y) {
   const R = Math.floor(Math.random() * 256)
   const G = Math.floor(Math.random() * 256)
   const B = Math.floor(Math.random() * 256)
-  const A = Math.floor(Math.random() * 50) / 100
-  newCircle.setAttribute("style", `
-    top: ${y}px;
-    left: ${x}px;
-    background: rgba(${R},${G},${B},${A});
-  `)
+  const A = Math.floor(Math.random() * 50 + 25) / 100
+  newCircle.style.top = `${y}px`
+  newCircle.style.left = `${x}px`
+  newCircle.style.background = `rgba(${R},${G},${B},${A})`
   elemBody.appendChild(newCircle)
   deleteElemTimeout(newCircle)
 }
